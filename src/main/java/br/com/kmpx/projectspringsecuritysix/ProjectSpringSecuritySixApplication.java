dad9ff20c0fa24a7180866181b395a5fa2de89e2
OdirleiK <odirlei67777@gmail.com>
2024-05-06 19:32:22 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/kmpx/projectspringsecuritysix/ProjectSpringSecuritySixApplication.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.projectspringsecuritysix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringSecuritySixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringSecuritySixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kmpx/projectspringsecuritysix/ProjectSpringSecuritySixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.projectspringsecuritysix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringSecuritySixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
